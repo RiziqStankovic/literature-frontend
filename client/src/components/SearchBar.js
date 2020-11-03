736f02627443808b11b1e9c35d371131be75643f
Nur Askiah <askiahnur1@gmail.com>
2020-11-04 01:09:47 +0800

    fix bug

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -13,49 +13,47 @@ const SearchBar = () => {
   const history = useHistory();
 
   return (
-    <div>
-      <Formik
-        validationSchema={schema}
-        onSubmit={(values) => {
-          history.push(`/search?q=${values.search}`);
-        }}
-        initialValues={{
-          search: '',
-        }}
-      >
-        {({ handleSubmit, handleChange, values, errors }) => (
-          <Form onSubmit={handleSubmit}>
-            <Form.Row>
-              <Form.Group>
-                <Form.Control
-                  type="text"
-                  value={values.search}
-                  name="search"
-                  placeholder="Search for literature"
-                  onChange={handleChange}
-                  style={{ width: 500 }}
-                  isInvalid={!!errors.search}
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a query in the search box above.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group>
-                <Button
-                  className="ml-2"
-                  type="submit"
-                  style={{
-                    padding: 5,
-                  }}
-                >
-                  <AiOutlineSearch size="26px" color="white" />
-                </Button>
-              </Form.Group>
-            </Form.Row>
-          </Form>
-        )}
-      </Formik>
-    </div>
+    <Formik
+      validationSchema={schema}
+      onSubmit={(values) => {
+        history.push(`/search?q=${values.search}`);
+      }}
+      initialValues={{
+        search: '',
+      }}
+    >
+      {({ handleSubmit, handleChange, values, errors }) => (
+        <Form onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                value={values.search}
+                name="search"
+                placeholder="Search for literature"
+                onChange={handleChange}
+                style={{ width: 500 }}
+                isInvalid={!!errors.search}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a query in the search box above.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Button
+                className="ml-2"
+                type="submit"
+                style={{
+                  padding: 5,
+                }}
+              >
+                <AiOutlineSearch size="26px" color="white" />
+              </Button>
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
